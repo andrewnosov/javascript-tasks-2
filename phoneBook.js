8c98c4a6546a96128264428c9703768dd6889cc1
andrewnosov <andrei-ais2@yandex.ru>
2015-10-11 21:23:33 +0500

    js-2

--- phoneBook.js
+++ phoneBook.js
@@ -1,15 +1,35 @@
 'use strict';
 
-var phoneBook; // Здесь вы храните записи как хотите
+var phoneBook = []; // Здесь вы храните записи как хотите
 
 /*
-   Функция добавления записи в телефонную книгу.
+   Функция добавления запси в телефонную книгу.
    На вход может прийти что угодно, будьте осторожны.
 */
+
+var emailValid = function(email){
+    var e = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
+    return e.test(email);
+};
+
+var phoneValid = function(phone){
+    var p = /^[0-9\+\-\(\)\s]+$/;
+    return p.test(phone);
+  };
+
 module.exports.add = function add(name, phone, email) {
 
     // Ваша невероятная магия здесь
-
+if(phoneValid(phone) && emailValid(email))
+{
+    var note = {
+    name: name,
+    phone: phone,
+    email: email
+}
+    phoneBook.push(note);
+    console.log("Добавлено")}else{
+    console.log("Не добавлено");}
 };
 
 /*
@@ -19,7 +39,13 @@ module.exports.add = function add(name, phone, email) {
 module.exports.find = function find(query) {
 
     // Ваша удивительная магия здесь
-
+for(var i=0; i<phoneBook.length; i++){
+    if(phoneBook[i].name.indexOf(query) !== -1 ||
+    phoneBook[i].phone.indexOf(query) !== -1 ||
+    phoneBook[i].email.indexOf(query) !== -1){
+    console.log(phoneBook[i].name + ", " + phoneBook[i].phone + ", " + phoneBook[i].email)
+    }
+    }
 };
 
 /*
@@ -28,25 +54,13 @@ module.exports.find = function find(query) {
 module.exports.remove = function remove(query) {
 
     // Ваша необьяснимая магия здесь
-
-};
-
-/*
-   Функция импорта записей из файла (задача со звёздочкой!).
-*/
-module.exports.importFromCsv = function importFromCsv(filename) {
-    var data = require('fs').readFileSync(filename, 'utf-8');
-
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
-};
-
-/*
-   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
-*/
-module.exports.showTable = function showTable() {
-
-    // Ваша чёрная магия здесь
-
+var delCount = 0;
+for(var i = 0; i<phoneBook.length; i++){
+    if(phoneBook[i].name.indexOf(query) !== -1 ||
+    phoneBook[i].phone.indexOf(query) !== -1 ||
+    honeBook[i].email.indexOf(query) !== -1){
+    phoneBook.splice(i,1);
+    delCount++;
+    }
+    }
 };
